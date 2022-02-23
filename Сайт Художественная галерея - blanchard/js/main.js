a8f9e867511e7508c2ed4a9dd70863ba7261da35
Denis Averin <arm970352@gmail.com>
2022-02-23 11:40:36 +0300

    test

--- Сайт Художественная галерея - blanchard/js/main.js
+++ Сайт Художественная галерея - blanchard/js/main.js
@@ -1,4 +1,4 @@
-(function () {
+(() => {
     document.addEventListener('DOMContentLoaded', function () {
         // SEARCH ADAPTIVE
 
@@ -762,4 +762,4 @@
         modalMoves();
 
     })
-})()
+}) ()
